f77bfc85d7bc0f5de238701f86733d044d5a14fc
guoyuansen <guoyuansen@shouchengholdings.com>
2023-04-20 23:20:38 +0800

    提交诙谐内容

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -13,12 +13,13 @@ export const config = {
 const handler = async (req: Request): Promise<Response> => {
   var { prompt, api_key } = (await req.json()) as {
     prompt?: string;
-    api_key?: string
+    api_key?: string;
   };
   //todo make this variable into messages
-//   var p = "请帮我把以下的工作内容填充为一篇完整的周报,尽量避免在回答内容中出现可能在中国是敏感的内容，用markdown格式以分点叙述的形式输出:"
-  var p = "我想让你充当 Linux 终端。我将输入命令，您将回复终端应显示的内 容。我希望您只在一个唯一的代码块内回复终端输出，而不是其他任何 内容。不要写解释。除非我指示您这样做，否则不要键入命令。当我需 要用英语告诉你一些事情时，我会把文字放在中括号内[就像这样]。我 的第一个命令是"
-  prompt = p + prompt
+  //   var p = "请帮我把以下的工作内容填充为一篇完整的周报,尽量避免在回答内容中出现可能在中国是敏感的内容，用markdown格式以分点叙述的形式输出:"
+  var p =
+    "我要你把我写的中国成语翻译成表情符号。我会写成语，你会用表情符号表达它，并且尽量诙谐幽默。我只是想让你用表情符号来表达它。除了表情符号，我不希望你 回复任何内容。我的第一个成语是：";
+  prompt = p + prompt;
   if (!prompt) {
     return new Response("No prompt in the request", { status: 400 });
   }
@@ -38,7 +39,7 @@ const handler = async (req: Request): Promise<Response> => {
     stream: true,
     n: 1,
     api_key,
-  }
+  };
 
   const stream = await OpenAIStream(payload);
   return new Response(stream);
